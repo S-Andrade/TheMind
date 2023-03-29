ad42c86d03189a9c0acf11459182556f4363e035
S-Andrade <sandraandrade@ua.pt>
2023-03-29 18:20:08 +0100

    runall

--- runall.bat
+++ runall.bat
@@ -1,8 +1,9 @@
-start ThalamusGameMasterClient\bin\Debug\ConsoleApplication.exe Table SERA 192.168.0.101 7000
-start ThalamusTabletClient\bin\Debug\ConsoleApplication.exe Tablet0 SERA 192.168.0.102 7030
-start ThalamusTabletClient\bin\Debug\ConsoleApplication.exe Tablet1 SERA 192.168.0.103 7040
-start ThalamusTabletClient\bin\Debug\ConsoleApplication.exe Tablet2 SERA 192.168.0.104 7050
+start ThalamusGameMasterClient\bin\Debug\ConsoleApplication.exe Table SERA 192.168.0.100 7000
+start ThalamusTabletClient\bin\Debug\ConsoleApplication.exe Tablet0 SERA 192.168.0.101 7030
+start ThalamusTabletClient\bin\Debug\ConsoleApplication.exe Tablet1 SERA 192.168.0.102 7040
+start ThalamusTabletClient\bin\Debug\ConsoleApplication.exe Tablet2 SERA 192.168.0.103 7050
 
+//start the-mind-mainscreen\build\TheMindMainScreen
 
 
 
